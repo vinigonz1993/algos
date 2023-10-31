37e5bc03d36a4519acd4746d3fb594d41269095e
vinigonz1993 <vinigonzalez1993@gmail.com>
2023-10-31 13:46:49 -0400

    Finding a corrupted node

---
+++ 2/2.8.py
@@ -0,0 +1,34 @@
+# Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the
+# beginning of the loop.
+# DEFINITION
+# Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so
+# as to make a loop in the linked list.
+# EXAMPLE
+# Input:A - > 8 - > C - > D - > E - > C [the same C as earlier]
+# Output:C
+
+from linked_list import LinkedList
+
+class CustomLinkedList(LinkedList):
+
+    def get_corrupted_node(self):
+        values = []
+
+        current = self.head
+        corrupt_node = None
+
+        while current.next:
+            if current.data in values:
+                corrupt_node = current
+                break
+            values.append(current.data)
+            current = current.next
+        return corrupt_node
+
+linked_list = CustomLinkedList()
+
+linked_list.generate_list([1, 2, 3, 4, 5, 6, 7, 8, 6, 9, 0])
+
+linked_list.display()
+print()
+print(linked_list.get_corrupted_node())
